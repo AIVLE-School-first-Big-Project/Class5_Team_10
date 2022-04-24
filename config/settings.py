8355cc9aceaf32995d543b43c5277660c1cf3a35
heejun32 <rlaskan95@naver.com>
2022-04-24 11:33:43 +0900

    게시물 수정 페이지 완료

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
     'user',
     'board',
     'meal',
@@ -137,3 +138,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # LOGIN_REDIRECT_URL = ''
+
