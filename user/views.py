7a194d8331968147bec3b9df309e15a3eb0d8eb1
junghyun11 <lear01@naver.com>
2022-05-06 13:53:52 +0900

    User footer 수정

--- user/views.py
+++ user/views.py
@@ -8,11 +8,10 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from django.forms import formset_factory, inlineformset_factory
-from user.forms import UserCreationForm, KidRegisterForm, UpdateUserForm, UpdateKidForm
+from user.forms import UserCreationForm, KidRegisterForm
+from user.forms import UpdateUserForm, UpdateKidForm
 from user.models import User, Kid
-from django.views.decorators.csrf import csrf_exempt
-from user.decorators import *
+from user.decorators import login_message_required, logout_message_required
 
 
 # 회원가입
@@ -20,42 +19,44 @@ from user.decorators import *
 def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+        context = {'form': form}
         id = request.POST.get('id')
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
         email = request.POST.get('email')
-        name = request.POST.get('name')      
+        name = request.POST.get('name')
         # 사용자 아이디가 이미 존재하는 아이디인 경우
-        try: 
+        try:
             if User.objects.get(id=id):
                 id_error_msg = '이미 존재하는 아이디입니다.'
-            context={
-                'form' : form,
-                'id_error_msg' : id_error_msg,
+            context = {
+                'form': form,
+                'id_error_msg': id_error_msg,
             }
-            return render(request, 'user/signup.html', context)        
+            return render(request, 'user/signup.html', context) 
         except:
             # 비밀번호와 비밀번호 확인이 일치하지 않는 경우
             if password1 != password2:
                 password_error_msg = '비밀번호가 일치하지 않습니다. 다시 입력해주세요.'
-                return render(request, 'user/signup.html', {'form': form, 'password_error_msg': password_error_msg})
+                return render(request, 'user/signup.html',
+                    {'form': form, 'password_error_msg': password_error_msg})
             else:
                 if form.is_valid():
-                    User.objects.create_user(id=id, username=id, password=password1, email=email, name=name)
-                    # user = authenticate(username=id, password=password1)  # 사용자 인증
-                    # auth_login(request, user)  # 로그인
+                    User.objects.create_user(id=id, username=id,
+                        password=password1, email=email, name=name)
                     return redirect('user:login')
                 else:
                     email_error_msg = '이메일 양식이 맞지 않습니다.'
                     context = {
-                        'form' : form,
-                        'email_error_msg' : email_error_msg,
+                        'form': form,
+                        'email_error_msg': email_error_msg,
                     }
-                    return render(request, 'user/signup.html', {'form': form, 'email_error_msg': email_error_msg})
+                    return render(request, 'user/signup.html',
+                        {'form': form, 'email_error_msg': email_error_msg})
     else:
         form = UserCreationForm()
-    context={
-        'form' : form,
+    context = {
+        'form': form,
     }
     return render(request, 'user/signup.html', context)
 
@@ -65,12 +66,12 @@ def id_check(request):
     id = request.GET.get('user_id')
     result = 'fail'
     try:
-        check_result =  User.objects.get(id=id)
+        check_result = User.objects.get(id=id)
     except:
         check_result = None
     if check_result is None:
         result = 'possible'
-    return JsonResponse({ 'result': result })
+    return JsonResponse({'result': result})
 
 
 # 로그인
@@ -87,21 +88,21 @@ def CustomLogin(request):
             else:
                 password_error_msg = '비밀번호가 일치하지 않습니다. 다시 입력해주세요.'
                 context = {
-                    'form' : form,
-                    'password_error_msg' : password_error_msg,
+                    'form': form,
+                    'password_error_msg': password_error_msg,
                 }
                 return render(request, 'user/login.html', context)
         except:
             id_error_msg = '일치하는 아이디가 없습니다.'
             context = {
-                    'form' : form,
-                    'id_error_msg' : id_error_msg,
+                    'form': form,
+                    'id_error_msg': id_error_msg,
             }
             return render(request, 'user/login.html', context)
     else:
         form = AuthenticationForm()
     context = {
-        'form' : form,
+        'form': form,
     }
     return render(request, 'user/login.html', context)
 
@@ -111,7 +112,7 @@ def CustomLogin(request):
 def kid_register(request):
     if request.method == "POST":
         form = KidRegisterForm(request.POST, request.FILES)
-        context = {'form' : form}
+        context = {'form': form}
         try:
             form.errors['birthday']
             birthday_error_msg = "올바른 생년월일을 입력하세요"
@@ -149,9 +150,9 @@ def kid_register(request):
 def kid_select(request):
     if request.user.is_authenticated:
         user_id = request.session['_auth_user_id']
-        kid_set = Kid.objects.filter(user_id = user_id)
+        kid_set = Kid.objects.filter(user_id=user_id)
     context = {
-        'kid_set' : kid_set,
+        'kid_set': kid_set,
     }
     print(kid_set)
     return render(request, 'user/select_kid.html', context)
@@ -182,15 +183,15 @@ def user_update(request):
             else:
                 email_error_msg = '이메일 양식이 맞지 않습니다.'
                 context = {
-                    'form' : form,
-                    'email_error_msg' : email_error_msg,
+                    'form': form,
+                    'email_error_msg': email_error_msg,
                 }
-                return render(request, 'user/user_update.html', context)  
+                return render(request, 'user/user_update.html', context)
     else:
         form = UpdateUserForm()
     context = {
-        'form' : form,
-        'user' : user,
+        'form': form,
+        'user': user,
     }
     return render(request, 'user/user_update.html', context)
 
@@ -221,8 +222,9 @@ def ForgotIDView(request):
         try:
             user = User.objects.get(email=email)
             if user is not None:
-                template = render_to_string('user/find_username_email_template.html', 
-                                            {'name': user.name, 'id':user.username})
+                template = render_to_string('user/find_username_email_template.html',
+                                        {'name': user.name,
+                                        'id': user.username})
                 method_email = EmailMessage(
                     '[밀키드] 요청하신 ID입니다.',
                     template,
@@ -234,7 +236,7 @@ def ForgotIDView(request):
         except:
             email_error_msg = '일치하는 이메일이 없습니다.'
             context = {
-                'email_error_msg' : email_error_msg,
+                'email_error_msg': email_error_msg,
             }
             return render(request, 'user/find_username_form.html', context)
     context = {}
@@ -244,7 +246,7 @@ def ForgotIDView(request):
 # 아이 정보 삭제
 @login_message_required
 def kid_del(request):
-    kid_id = request.session['kid_id'] # 아이 세션 정보를 받아와야 함
+    kid_id = request.session['kid_id']
     data = Kid.objects.get(pk=kid_id)
     data.delete()
     return redirect('user:kid_select')
@@ -264,16 +266,16 @@ def kid_update_each(request):
     else:
         form = UpdateKidForm(instance=kid)
     context = {
-        'form' : form,
-        'kid' : kid,
+        'form': form,
+        'kid': kid,
     }
     return render(request, 'user/kid_update_each.html', context)
 
-    
+
 # 아이 선택 시 세션에 저장
 @csrf_exempt
 def kid_send(request):
     request.session['kid_id'] = 0
     if request.session['kid_id'] == 0:
         request.session['kid_id'] = request.POST.get('kid_id')
-    return  render(request, 'user/user_update.html')
+    return render(request, 'user/user_update.html')
