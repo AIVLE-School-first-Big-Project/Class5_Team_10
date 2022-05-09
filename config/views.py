07a364c980ade87ba033cebd5f7d0d9446f2d13a
songjeongwoo <nice920525@naver.com>
2022-05-09 13:51:59 +0900

    render_to_response -> render로 변경(확장성 고려)

--- config/views.py
+++ config/views.py
@@ -1,5 +1,4 @@
 from django.template.context_processors import csrf
-from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
@@ -21,13 +20,13 @@ def server_error(request):
 def any_request(request):
     context = {}
     context.update(csrf(request))
-    return render_to_response('any_request.html', context=context)
+    return render(request, 'any_request.html', context=context)
 
 
 # 403 에러(2)
 def csrf_failure(request, reason=""):
     auth_logout(request)
     context = {'RequestContext': RequestContext(request)}
-    response = render_to_response('403.html', context)
+    response = render(request, '403.html', context)
     response.status_code = 403
     return response
